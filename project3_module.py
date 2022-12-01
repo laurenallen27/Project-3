01a5af5893e9586d4770d5479ccd2c528c09e00a
laurenallen27 <lvallen@uvm.edu>
2022-12-01 10:00:49 -0500

    Created files

---
+++ project3_module.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 09:51:14 2022
+
+Project 3: Module 
+
+@author: laurenallen, altagodfrey
+"""
+
+Hello
---
+++ project3_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 09:52:31 2022
+
+Project 3: Script
+
+@author: laurenallen, altagodfrey
+"""
+
