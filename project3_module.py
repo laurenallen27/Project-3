d1c5848e24211fbeb9e50618597d0977526e6241
laurenallen27 <lvallen@uvm.edu>
2022-12-01 11:05:36 -0500

    Part 1
    Created plots

--- project3_module.py
+++ project3_module.py
@@ -15,12 +15,25 @@ from scipy import fft
 
 #%% Part 1: Collect and Load Data
 # Create function to load data for 4 different activity categories
+def load_data(input_file):
+    data_file = np.loadtxt(input_file, dtype = float)
+    
+    return data_file
+    
 
 #%% Part 2: Filter Your Data
 # Create a function to apply to filter to each dataset
 
 #%% Part 3: Detect Heartbeats
+# Create function to detect heartbeats in each dataset
 
 #%% Part 4: Calculate Heart Rate Variability
+# Create function to calculate the inter-beat intervals from detected heartbeats
+
+# Create function to calculate one HRV measure for each activity
 
 #%% Part 5: Get HRV Frequency Band Power
+# Create function to calculate the frequency domain magnitude of each activity’s IBI timecourse signal
+
+#Create function to extract the mean power in the LF and HF frequency bands & calculate the LF/HF ratio for each
+
--- project3_script.py
+++ project3_script.py
@@ -12,31 +12,55 @@ Project 3: Script
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fft
+import project3_module as p3m
 
 #%% Part 1: Collect and Load Data
 # Call the load data function for each activity
+rest_data_file = p3m.load_data('rest_data (1).txt')
+relaxing_data_file = p3m.load_data('on_phone_data (1).txt')
+stress_rest_data_file = p3m.load_data('stressful_rest (1).txt')
+physical_data_file = p3m.load_data('wallsit_data (1).txt')
 
-# Create 5 plots (maybe make a function for this?)
-
-# Plot of 4 activity recordings concatenated together
 
 # Plot of 5 minutes sitting at rest
+plt.figure(1)
+plt.subplot(2,2,1)
+plt.plot(rest_data_file)
 
 # Plot of 5 minutes of relaxing activity
+plt.subplot(2,2,2)
+plt.plot(relaxing_data_file)
 
 # Plot of 5 minutes of mentally stressful activity
+plt.subplot(2,2,3)
+plt.plot(stress_rest_data_file)
 
 # Plot of 5 minutes of physically stressful activity
+plt.subplot(2,2,4)
+plt.plot(physical_data_file)
+
+# Plot of 4 activity recordings concatenated together
+# concatenate 4 signals
+concatenated_data = np.concatenate((rest_data_file, relaxing_data_file, stress_rest_data_file, physical_data_file))
+plt.figure(2)  
+plt.plot(concatenated_data)                         
 
 #%% Part 2: Filter Your Data
-# Create filter
+# Create filter to get rid of low frequency drift and high frequency fuzz
 
 #Plot filter's impulse response and frequency response
 
 # Plot data from one activity before and after filter is applied
 
 #%% Part 3: Detect Heartbeats
+# Plot data for each activity's detected heartbeat times
 
 #%% Part 4: Calculate Heart Rate Variability
+# Plot HRV measure for each activity in a bar graph (activity on x axis, HRV on y axis)
+
+# Calculate an interpolated timecourse of IBIat regular intervalsof dt=0.1 seconds
 
 #%% Part 5: Get HRV Frequency Band Power
+# Plot frequency domain magnitude in power
+
+# Plot ratios of LF/HF in a bar graph
