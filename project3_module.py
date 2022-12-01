a698edc67cf84bc7c5e7dc216e7244ea257f94a9
laurenallen27 <lvallen@uvm.edu>
2022-12-01 10:33:32 -0500

    Pseudocode

--- project3_module.py
+++ project3_module.py
@@ -8,5 +8,19 @@ Project 3: Module
 @author: laurenallen, altagodfrey
 """
 
-Hello
-Does this work
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fft
+
+#%% Part 1: Collect and Load Data
+# Create function to load data for 4 different activity categories
+
+#%% Part 2: Filter Your Data
+# Create a function to apply to filter to each dataset
+
+#%% Part 3: Detect Heartbeats
+
+#%% Part 4: Calculate Heart Rate Variability
+
+#%% Part 5: Get HRV Frequency Band Power
--- project3_script.py
+++ project3_script.py
@@ -8,3 +8,35 @@ Project 3: Script
 @author: laurenallen, altagodfrey
 """
 
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fft
+
+#%% Part 1: Collect and Load Data
+# Call the load data function for each activity
+
+# Create 5 plots (maybe make a function for this?)
+
+# Plot of 4 activity recordings concatenated together
+
+# Plot of 5 minutes sitting at rest
+
+# Plot of 5 minutes of relaxing activity
+
+# Plot of 5 minutes of mentally stressful activity
+
+# Plot of 5 minutes of physically stressful activity
+
+#%% Part 2: Filter Your Data
+# Create filter
+
+#Plot filter's impulse response and frequency response
+
+# Plot data from one activity before and after filter is applied
+
+#%% Part 3: Detect Heartbeats
+
+#%% Part 4: Calculate Heart Rate Variability
+
+#%% Part 5: Get HRV Frequency Band Power
